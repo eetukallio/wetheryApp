95b433c3865ff01990e7683c70d7d65d0fd566fb
eetukallio <eetu.kallio@gmail.com>
2018-10-04 11:16:32 +0300

    Initial Commit

---
+++ WeatheryApp/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  WeatheryApp
+//
+//  Created by Eetu Kallio on 04/10/2018.
+//  Copyright © 2018 Eetu Kallio. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ WeatheryApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  WeatheryApp
+//
+//  Created by Eetu Kallio on 04/10/2018.
+//  Copyright © 2018 Eetu Kallio. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
